e5f2ab0f189810fb22a5427df1f32d6f5e3f5a82
Divya Panchal <dpanchal@yahoo.com>
2016-05-31 19:01:05 -0700

    My First Attempt

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Assignment: Caching the Inverse of a Matrix
+## Pair of functions that cache the inverse of a matrix.
 
-## Write a short comment describing this function
+##makeCacheMatrix function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) inv <<- inverse
+    getinverse <- function() inv
+    list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinverse()
+    if(!is.null(inv)) {
+        message("Retrieving cached data.")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data)
+    x$setinverse(inv)
+    inv
 }
+
+##Sample Run from R
+##x <- matrix(1:4, nrow=2, ncol=2)
+##m <- makeCacheMatrix(x)
+##s <- cacheSolve(m)
+##x
+##     [,1] [,2]
+##[1,]    1    3
+##[2,]    2    4
+
+##s
+##     [,1] [,2]
+##[1,]   -2  1.5
+##[2,]    1 -0.5
+##s2 <- cacheSolve(m)
+##Retrieving cached data.
+##s2
+ ##    [,1] [,2]
+##[1,]   -2  1.5
+##[2,]    1 -0.5
+ 
+
